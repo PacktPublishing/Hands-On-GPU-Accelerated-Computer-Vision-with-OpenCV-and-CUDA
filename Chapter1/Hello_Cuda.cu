a134d07c074956be221042dce4e9e8357aac28b4
bhaumik2450 <vaidya.bhaumik@gmail.com>
2018-05-03 16:16:59 +0530

    Add files via upload

--- Chapter1/Hello_Cuda.cu
+++ Chapter1/Hello_Cuda.cu
@@ -1,9 +1,11 @@
 #include <iostream>
 
-
+#include <stdio.h>
+__global__ void myfirstkernel(void) {
+}
 
 int main(void) {
-	
-	printf("Hello, world!\n");
+	myfirstkernel << <1, 1 >> >();
+	printf("Hello, CUDA!\n");
 	return 0;
 }
