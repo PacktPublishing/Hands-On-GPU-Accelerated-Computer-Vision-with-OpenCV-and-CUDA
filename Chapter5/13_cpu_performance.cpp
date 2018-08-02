b3a54cddd48be39f90ab78823be4df20f693d964
bhaumik2450 <vaidya.bhaumik@gmail.com>
2018-08-02 17:33:27 +0530

    Add files via upload

--- Chapter5/13_cpu_performance.cpp
+++ Chapter5/13_cpu_performance.cpp
@@ -8,11 +8,11 @@ cv::Mat src = cv::imread("images/cameraman.tif", 0);
 cv::Mat result_host1,result_host2,result_host3,result_host4,result_host5;
 //Get initial time in miliseconds
 int64 work_begin = getTickCount(); 
-cv::cuda::threshold(src, result_host1, 128.0, 255.0, cv::THRESH_BINARY);
-cv::cuda::threshold(src, result_host2, 128.0, 255.0, cv::THRESH_BINARY_INV);
-cv::cuda::threshold(src, result_host3, 128.0, 255.0, cv::THRESH_TRUNC);
-cv::cuda::threshold(src, result_host4, 128.0, 255.0, cv::THRESH_TOZERO);
-cv::cuda::threshold(src, result_host5, 128.0, 255.0, cv::THRESH_TOZERO_INV);
+cv::threshold(src, result_host1, 128.0, 255.0, cv::THRESH_BINARY);
+cv::threshold(src, result_host2, 128.0, 255.0, cv::THRESH_BINARY_INV);
+cv::threshold(src, result_host3, 128.0, 255.0, cv::THRESH_TRUNC);
+cv::threshold(src, result_host4, 128.0, 255.0, cv::THRESH_TOZERO);
+cv::threshold(src, result_host5, 128.0, 255.0, cv::THRESH_TOZERO_INV);
 //Get time after work has finished     
 int64 delta = getTickCount() - work_begin;
 //Frequency of timer
