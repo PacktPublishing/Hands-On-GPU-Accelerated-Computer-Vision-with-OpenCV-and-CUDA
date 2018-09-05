4013c290c0e6544c1e9cb12e6d9be017ebcfaba9
bhaumik2450 <vaidya.bhaumik@gmail.com>
2018-09-05 21:24:20 +0530

    Add files via upload

--- Chapter6/09_sobel_filter.cpp
+++ Chapter6/09_sobel_filter.cpp
@@ -9,9 +9,9 @@ int main ()
 	d_img1.upload(h_img1);
 	cv::Ptr<cv::cuda::Filter> filterx,filtery,filterxy;
    	filterx = cv::cuda::createSobelFilter(CV_8UC1,CV_8UC1,1,0);
-	filterx->apply(d_img1, d_resultx, stream);
+	filterx->apply(d_img1, d_resultx);
 	filtery = cv::cuda::createSobelFilter(CV_8UC1,CV_8UC1,0,1);
-	filtery->apply(d_img1, d_resulty, stream);
+	filtery->apply(d_img1, d_resulty);
 	cv::cuda::add(d_resultx,d_resulty,d_resultxy);        
 	cv::Mat h_resultx,h_resulty,h_resultxy;
     d_resultx.download(h_resultx);
